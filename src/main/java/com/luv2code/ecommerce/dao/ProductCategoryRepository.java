2511a3d8df37e3642fa02443f11718d468bd6f63
Naveen <ull.rac1@gmail.com>
2020-04-28 19:13:32 +0530

    CrossOrigin added

--- src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.luv2code.ecommerce.entity.ProductCategory;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 	@Override
